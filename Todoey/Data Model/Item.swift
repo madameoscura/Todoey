5f7e00c01677c0bb70a5fba673cc25d735627f63
Annekatrin Dunkel <annekatrin.dunkel@gmx.de>
2019-09-11 19:02:41 -0600

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Annekatrin Dunkel on 9/11/19.
+//  Copyright © 2019 Annekatrin Dunkel. All rights reserved.
+//
+
+import Foundation
+
+class Item {    
+    var title : String = ""
+    var done : Bool = false
+}
