1b8f8c88f4d5c661b489f1739cbf326d83b7e8c9
Annekatrin Dunkel <annekatrin.dunkel@gmx.de>
2019-12-29 15:59:45 +0100

    colors changed according to category

---
+++ Todoey/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Annekatrin Dunkel on 9/11/19.
+//  Copyright © 2019 Annekatrin Dunkel. All rights reserved.
+//
+
+import UIKit
+
+class Item: UIViewController {
+
+    let title = ""
+    let done : Bool = false
+    
+
+}
